3d0a5a04fc68c38c987ae19755b71f469d448017
mahmudxfiber <153246291+mahmudxfiber@users.noreply.github.com>
2023-12-15 21:55:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -194,16 +194,13 @@
               Products
             </h6>
             <p>
-              <a href="#!" class="text-reset">Angular</a>
+              <a href="#!" class="text-reset">Banyumas</a>
             </p>
             <p>
-              <a href="#!" class="text-reset">React</a>
+              <a href="#!" class="text-reset">Purwokerto</a>
             </p>
             <p>
-              <a href="#!" class="text-reset">Vue</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Laravel</a>
+              <a href="#!" class="text-reset">Ajibarang</a>
             </p>
           </div>
           <!-- Grid column -->
@@ -233,13 +230,12 @@
           <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
             <!-- Links -->
             <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
-            <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+            <p><i class="fas fa-home me-3"></i> Purwokerto,Jawa Tengah,Indonesia</p>
             <p>
               <i class="fas fa-envelope me-3"></i>
-              info@example.com
+              kraftaindonesia.com
             </p>
-            <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
-            <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+            <p><i class="fas fa-print me-3"></i> +6282322714828</p>
           </div>
           <!-- Grid column -->
         </div>
